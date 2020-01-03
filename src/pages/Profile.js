490ce3815589baaf680747ce1ebecbd7e0c23657
ShariyarAbeer <007abeershariyar@gmail.com>
2020-01-03 06:03:01 +0600

    warking

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,19 +5,7 @@ class Profile extends Component {
   render() {
     return (
       <div className="ProfilePage">
-        <h1>Profile</h1>
-        <h2>Question:</h2>
-        <ul>
-          <li>Where is your Profile?</li>
-        </ul>
-        <h2>Answer:</h2>
-        <p>
-          A file or group of files stored on a user. These files may contain
-          preferences, settings, contact information, credit information, etc. A
-          profile allows the user's settings to be restored when that user logs
-          back into the computer, network, or service.
-        </p>
-        <p>Also see: BIO, Internet terms, Profile site, User, User profile</p>
+        <h1>Md.Arif Shariyar</h1>
       </div>
     );
   }
