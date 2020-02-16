570cda4e7c1d7d5ee90ddc31f9f468a509e19082
ShariyarAbeer <007abeershariyar@gmail.com>
2020-02-16 22:30:02 +0600

    edu update, profile update, color chng

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Row, Col, initial } from "react";
+import { Card, Avatar, Divider } from "antd";
 
 class Home extends Component {
   state = {};
   render() {
     return (
       <div className="HomePage">
-        <h1>sample</h1>
+        <img
+          width={200}
+          height={200}
+          className="mr-7"
+          src="https://scontent.fdac15-1.fna.fbcdn.net/v/t1.0-9/29497614_815393248852087_9185027329996104120_n.jpg?_nc_cat=100&_nc_ohc=kDJdbIvBcZoAX-CytLd&_nc_ht=scontent.fdac15-1.fna&oh=7d3281b224f119daf1cc35134b630d11&oe=5EF8788A"
+          alt="Generic placeholder"
+        />
+        <h1>Md.Arif Shariyar</h1>
+
+        <Card>
+          Hi. I’m Md.Arif SHariyar Abeer, a developer from Dhaka, Bangladesh. I
+          am passionate about building excellent apps and website that improves
+          the lives of those around me. I specialize in creating apps and
+          website for clients ranging from individuals and small-businesses all
+          the way to large enterprise corporations. All of my work is produced
+          locally from Dhaka.
+        </Card>
       </div>
     );
   }
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,50 +1,21 @@
 import React, { Component } from "react";
+import { Timeline } from "antd";
 
 class Post extends Component {
   state = {};
   render() {
     return (
       <div className="PostPage">
-        <h1>Post</h1>
-        <h2>Question:</h2>
-        <ul>
-          <li>What Is a Facebook Post?</li>
-        </ul>
-        <h2>Answer:</h2>
-        <p>
-          A Facebook post or "status update" is a message in a special delivery
-          cyber-bottle. It is a comment, picture or other media that is posted
-          on the user's Facebook page or "wall." A Little Bit Longer Unlike
-          Tweets, Facebook posts aren't limited to 140 characters. They can be
-          as long as a paragraph and illustrated with pictures and videos. Users
-          can post directly to Facebook via smart phones or uploaded from a
-          computer. As with any social media platform, there is a method to
-          creating a successful Facebook post.
-          <li>7 Qualities of a Bangin' Facebook</li>
-          Post Facebook posts are about invoking engagement and action.
-          According to SocialApps HQ (the largest Facebook app store) (2012), an
-          excellent Facebook post has the following 7 qualities:
-          <li>It grabs! your reader's attention.</li>
-          <li>Has "no brainer" text</li>
-          <li>
-            Contains a call for action, such as asking a question or requesting
-            that you like something.
-          </li>
-          <li>
-            Is appropriate for the demographics and geographic location of your
-            fan base. (Yes, demographics and geography are important on the
-            web.){" "}
-          </li>
-          <li>
-            It contains content that 95% of your audience cares (or better yet -
-            is passionate) about.{" "}
-          </li>
-          <li>
-            Sells the dream of your company and fan base (If you don't know
-            already, find out what that dream is.){" "}
-          </li>
-          <li>Is based on what you have learned from ad-testing</li>
-        </p>
+        <Timeline>
+          <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+          <Timeline.Item>
+            Solve initial network problems 2015-09-01
+          </Timeline.Item>
+          <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+          <Timeline.Item>
+            Network problems being solved 2015-09-01
+          </Timeline.Item>
+        </Timeline>
       </div>
     );
   }
