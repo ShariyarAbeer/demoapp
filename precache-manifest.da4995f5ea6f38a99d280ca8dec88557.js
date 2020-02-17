da079e8029a1750213b165083e9b156eac8e47ea
ShariyarAbeer <007abeershariyar@gmail.com>
2020-02-18 03:39:56 +0600

    Updates

---
+++ precache-manifest.da4995f5ea6f38a99d280ca8dec88557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63b0b01d071cee15e4ea",
+    "url": "/demoapp/static/css/main.74f81598.chunk.css"
+  },
+  {
+    "revision": "63b0b01d071cee15e4ea",
+    "url": "/demoapp/static/js/main.3169194a.chunk.js"
+  },
+  {
+    "revision": "89e560f96a04bc705af0",
+    "url": "/demoapp/static/js/runtime~main.0423dee1.js"
+  },
+  {
+    "revision": "aeb1eb05df2700fc4d47",
+    "url": "/demoapp/static/js/2.329230ff.chunk.js"
+  },
+  {
+    "revision": "b002a89220c32de6a24163a46f6f0d06",
+    "url": "/demoapp/index.html"
+  }
+];
